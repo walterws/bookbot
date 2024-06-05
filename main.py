7b7f533d6404ba3e1426d1db1ed2f788041191e5
walterws <walterws@gmail.com>
2024-06-04 21:03:00 -0300

    Exercicio 11.Report

--- main.py
+++ main.py
@@ -3,17 +3,34 @@ def main():
     file_contents=None
     words_count_output=None
     count_unique_caracters_output=None
+    count_unique_sorted=None
+
+    print("--- Begin report of books/frankenstein.txt ---")
 
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
     words_count_output = count_words(file_contents)
-
     count_unique_caracters_output = count_unique_caracters(file_contents)
 
-    #print(file_contents)
-    #print(words_count_output)
-    print(count_unique_caracters_output)
+    def value_getter(item):
+        return item[1]
+
+    count_unique_sorted=sorted(count_unique_caracters_output.items(), key=value_getter , reverse=True )
+
+    print(f"{words_count_output} words found in the document")
+    print("")
+
+    #print(count_unique_sorted)
+
+    for i in range(0, len(count_unique_sorted)):
+        if count_unique_sorted[i][0].isalpha():
+            print(f"The '{count_unique_sorted[i][0]}' character was found '{count_unique_sorted[i][1]}' times")
+
+    print("--- End report ---")
+
+
+
 
 def count_unique_caracters(texto_input):
     dir_caracteres={}
